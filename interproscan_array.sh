e1e17f51dc5ecded9b36a3b6b893c179fe5eed43
Austin B. Osmanski <austin.osmanski@gmail.com>
2025-02-10 15:56:29 -0600

    Update interproscan_array.sh

--- interproscan_array.sh
+++ interproscan_array.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-#SBATCH --job-name=6.6_IPS
+#SBATCH --job-name=IPS_sig
 #SBATCH --output=%x.%j.out
 #SBATCH --error=%x.%j.err
 #SBATCH --partition=nocona
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH -a 1-1790
+#SBATCH --cpus-per-task=8
+#SBATCH -a 1-91
+#SBATCH --mem=16G
 
-NAMESFILE=/lustre/scratch/aosmansk/clean_out/interproscan/SCORTHOS_6.6
+NAMESFILE=/lustre/scratch/aosmansk/crocs/selection_tests/interproscan/significant_SCORTHOS
 SCORTHO=$(awk "NR==$SLURM_ARRAY_TASK_ID" $NAMESFILE)
 
 ## Initialize conda
 source ~/conda/etc/profile.d/conda.sh
 conda init bash
-
-## Activate the TEclass2 environment
 conda activate biopython
 
-cd /lustre/scratch/aosmansk/clean_out/interproscan/workdirs/$SCORTHO/
+cd /lustre/scratch/aosmansk/crocs/selection_tests/interproscan/workdirs/$SCORTHO/
 
-/lustre/work/aosmansk/apps/interproscan/interproscan-5.67-99.0/interproscan.sh -i $SCORTHO.fas
+# Set Java memory options
+export JAVA_OPTS="-XX:+UseParallelGC -XX:ParallelGCThreads=8 -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -Xms1G -Xmx12G"
 
-#/lustre/scratch/aosmansk/clean_out/interproscan/workdirs/cPor_g10115.t1/cPor_g10115.t1.fas
+/lustre/work/aosmansk/apps/interproscan/interproscan-5.67-99.0/interproscan.sh \
+    -i $SCORTHO.sig_scortho.faa \
+    -cpu 8 \
+    -goterms \
+    -iprlookup \
+    -pa \
+    -f TSV,GFF3 \
+    -t p
